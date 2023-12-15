fe52f1c557c893d322522b5a6248e18c8bbbce08
saltonly <a.lu.nsk@mail.ru>
2023-12-15 14:25:25 +0700

    Initial project version

---
+++ Unyty Lab/Crazy_Hedgehogs2/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Il2CppMetadataRegistration.c
@@ -0,0 +1,41 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	4663,
+	g_Il2CppGenericTypes,
+	2846,
+	g_Il2CppGenericInstTable,
+	40924,
+	g_Il2CppGenericMethodFunctions,
+	14913,
+	g_Il2CppTypeTable,
+	48242,
+	g_Il2CppMethodSpecTable,
+	4730,
+	g_FieldOffsetTable,
+	4730,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
